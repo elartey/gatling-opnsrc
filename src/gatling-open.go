29c5701abbc7b7667d19081290508e85ce2334c3
Emmanuel Lartey <e.n.a.lartey@gmail.com>
2017-05-22 10:44:01 +0000

    recompiled binaries

--- src/gatling-open.go
+++ src/gatling-open.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"runtime"
 	"strings"
 	"time"
@@ -15,37 +16,38 @@ var client *http.Client
 var successful_count int
 var total_push int
 
-func gatling(url string, object string, xHeaders string, ch chan<- bool, rtype string) {
+func gatling(url string, object string, xHeaders string, ch chan<- bool, rtype string, otype string) {
 
 	count := 0
 	out := []byte(object)
-
 	cheads := strings.Fields(xHeaders)
-	for {
-		request, _ := http.NewRequest(rtype, url, bytes.NewBuffer(out))
-		for _, v := range cheads {
-			hs := strings.Split(v, ":")
-			request.Header.Set(hs[0], hs[1])
-		}
-		response, yoo := client.Do(request)
-		if yoo != nil {
-			log.Fatalf("[*] Error making request.", yoo)
-		}
-		defer response.Body.Close()
-
-		log.Printf("[*] Request complete! Finished Request No: #%v, Status: %v", count, response.StatusCode)
-
-		total_push += 1
-
-		count += 1
-
-		if response.StatusCode == 200 || response.StatusCode == 201 {
-			successful_count += 1
-			ch <- true
-		}
-
+	request, _ := http.NewRequest(rtype, url, bytes.NewBuffer(out))
+	if strings.ToLower(otype) != "xml" || strings.ToLower(otype) != "json" {
+		log.Printf("[*] Invalid content type. Please specify json or xml")
+		os.Exit(1)
+	} else {
+		request.Header.Set("Content-Type", "application/"+otype)
 	}
+	for _, v := range cheads {
+		hs := strings.Split(v, ":")
+		request.Header.Set(hs[0], hs[1])
+	}
+	response, yoo := client.Do(request)
+	if yoo != nil {
+		log.Fatalf("[*] Error making request.", yoo)
+	}
+	defer response.Body.Close()
 
+	log.Printf("[*] Request complete! Finished Request No: #%v, Status: %v", count, response.StatusCode)
+
+	total_push += 1
+
+	count += 1
+
+	if response.StatusCode == 200 || response.StatusCode == 201 {
+		successful_count += 1
+		ch <- true
+	}
 }
 
 func main() {
@@ -56,7 +58,6 @@ func main() {
 	urlString := flag.String("url", "", "Url to stress test e.g. 'http://acme.com'")
 	requestInt := flag.Int("rps", 0, "Number of requests to make simultaneously")
 	requestObject := flag.String("object", "", "Custom object to post e.g. {'foo':'bar'}")
-	//requestToken := flag.String("token", "", "Token to authorize requests")
 	objType := flag.String("objectType", "", "Type of object to post. e.g. 'xml' or 'json'")
 	numRequests := flag.Int("numR", 0, "Total number of requests to make")
 	reqType := flag.String("type", "", "HTTP request type you'd like to make. Currently supported type(s) 'POST'")
@@ -83,7 +84,7 @@ func main() {
 	default:
 		ch := make(chan bool)
 		for i := 0; i < *requestInt; i++ {
-			go gatling(*urlString, *requestObject, *heads, ch, *reqType)
+			go gatling(*urlString, *requestObject, *heads, ch, *reqType, *objType)
 		}
 
 		for r := 0; r < *numRequests; r++ {
